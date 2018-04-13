9db9eaa44eb7279f2b81bc52b449c149954471e0
0xShellcode <BDAthans@yahoo.com>
2018-04-13 17:00:42 -0500

    2 SQL Queries uploaded
    Two T SQL Queries added for selecting and updating eDocument path if EDocLink is only a filename, and does not contain a path.

---
+++ UpdatePath4DocsMissingPath.sql
@@ -0,0 +1,39 @@
+
+/* 
+SQL Query updates all eDocuments that are missing the file path in the EDocLink Attribute of table dbo.eDocuments
+
+NOTE: Update the @DataDir variable so that point to the eDocuments folder, and DO NOT end the DataDir with a \.
+This does not update any files that already include any pathing to the file. 
+This query only updates the EDocLinks that are only the filename.
+*/
+
+DECLARE @oldDocLink nvarchar(255);
+DECLARE @DataDir nvarchar(1000);
+DECLARE @docCount int = 0;
+SET @DataDir = '\\SERVER\Officemate\data\eDocuments';
+
+WHILE (SELECT COUNT(*) FROM eDocuments WHERE EDocLink LIKE '[0-9]%.%'
+OR EDocLink LIKE '[a-Z][a-Z]%.%' OR EDocLink LIKE '[a-Z][0-9]%.%') > 0
+BEGIN
+
+SELECT @oldDocLink = eDocuments.EDocLink
+FROM eDocuments
+WHERE EDocLink LIKE '[0-9]%.%'
+OR EDocLink LIKE '[a-Z][a-Z]%.%'
+OR EDocLink LIKE '[a-Z][0-9]%.%';
+
+UPDATE eDocuments
+SET EDocLink = @DataDir + '\' + @oldDocLink 
+WHERE eDocuments.EDocLink = @oldDocLink;
+
+print('Current DocLink = ' + @oldDocLink);
+print('Updated EDocLink = ' + @DataDir + @oldDocLink);
+SET @docCount += 1;
+
+END
+print('');
+print('Count of EDoclinks fixed below');
+print(@docCount);
+
+-- Might want to add logging function about what values were before and after update in loop
+
