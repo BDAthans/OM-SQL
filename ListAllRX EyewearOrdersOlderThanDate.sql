cd2893359b366485133a357a46f80a6d43a35c4f
0xShellcode <BDAthans@yahoo.com>
2019-08-01 11:17:12 -0500

    Add files via upload

--- ListAllRX EyewearOrdersOlderThanDate.sql
+++ ListAllRX EyewearOrdersOlderThanDate.sql
@@ -3,11 +3,10 @@
 DECLARE @removeBeforeThisDate nvarchar(1000) 
 SET @removeBeforeThisDate = '2018-12-31 00:00:00:000';
 
-SELECT rx_id, patient_id, promise_date, (patient.First_name +  ' ' + Patient.Last_name) as PatientName, 
+SELECT patient.first_name, patient.last_name, rx_id, patient_id, Ordered_By_Date, promise_date, 
 code.description as LabStatus, Rcvd_By_Date, Rcvd_By, Notified_By_Date, Notified_By, Delivered_By_Date, Delivered_By
 FROM (frame_rx INNER JOIN patient ON Patient.Patient_no=frame_rx.patient_id) LEFT JOIN Vendor ON vendor.vendor_no=frame_rx.order_job_where LEFT JOIN Code ON code.code=frame_rx.rx_order_cd
 WHERE promise_Date < @removeBeforeThisDate
 AND patient.active=1
 AND Ordered_By_Date IS NOT NULL
 AND Rcvd_By_Date is NULL
-
--- UpdateRXEyewearOrderstoDispensed.sql
+++ UpdateRXEyewearOrderstoDispensed.sql
@@ -1,16 +1,17 @@
 -- UPDATE query to update RX Eyewear Orders that are older than a specific date to be received, notified and dispensed (delivered).
 
 DECLARE @providerID int;
+DECLARE @removeBeforeThisDate nvarchar(1000);
+DECLARE @newOrderDate nvarchar(1000);
 SET @providerID = 1;
-
-DECLARE @removeBeforeThisDate nvarchar(1000) 
 SET @removeBeforeThisDate = '2018-12-31 00:00:00:000';
+SET @newOrderDate = '2019-07-11 00:00:00:000';
 
 
 Update frame_RX
-SET Rcvd_By_Date='2019-07-11 00:00:00:000', Rcvd_By=@providerID, 
-Notified_By_Date='2019-07-11 00:00:00:000', Notified_By=@providerID,
-Delivered_By_Date='2019-07-11 00:00:00:000', Delivered_By=@providerID
+SET Rcvd_By_Date=@newOrderDate, Rcvd_By=@providerID, 
+Notified_By_Date=@newOrderDate, Notified_By=@providerID,
+Delivered_By_Date=@newOrderDate, Delivered_By=@providerID
 FROM dbo.Frame_RX
 JOIN dbo.patient
 ON patient.patient_no=frame_RX.patient_id
@@ -18,6 +19,3 @@ WHERE promise_Date < @removeBeforeThisDate
 AND patient.active=1
 AND Ordered_By_Date IS NOT NULL
 AND Rcvd_By_Date is NULL
-
-
--- FROM (frame_rx INNER JOIN patient ON Patient.Patient_no=frame_rx.patient_id) LEFT JOIN Vendor ON vendor.vendor_no=frame_rx.order_job_where LEFT JOIN Code ON code.code=frame_rx.rx_order_cd
